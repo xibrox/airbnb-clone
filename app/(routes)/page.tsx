7935142df97c102d1516d2863bde186c6f228d20
xibrox <batonchik2004@gmail.com>
2023-07-21 19:09:40 +0200

    fix 2

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -6,6 +6,8 @@ import Container from "@/app/components/Container";
 import EmptyState from "@/app/components/EmptyState";
 import ListingCard from "@/app/components/listings/ListingCard";
 
+export const dynamic = "force-dynamic";
+
 interface HomeProps {
   searchParams: IListingsParams
 }
